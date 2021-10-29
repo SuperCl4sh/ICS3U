927e0b613a9272c4fd4477fdd062e67a0bc258a3
Seyon Sivatharan <superclash554@gmail.com>
2021-10-29 10:00:11 -0400

    Fix ics3ucompassu2assa (level 2 compass) to match problem output

--- Unit 2 (Selection)/ics3ucompassu2assa.java
+++ Unit 2 (Selection)/ics3ucompassu2assa.java
@@ -5,10 +5,13 @@ public class ics3ucompassu2assa {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		if (0 <= N && N <= 45) System.out.println("N");
-		else if (N < 135) System.out.println("E");
-		else if (N <= 225) System.out.println("S");
-		else if (N < 360) System.out.println("W");
-		else System.out.println("Invalid");
+		if (N < 0 || N >= 360) System.out.println("Invalid");
+		else {
+			System.out.print("Bearing " + N + " is closest to ");
+			if (0 <= N && N <= 45) System.out.println("North");
+			else if (N < 135) System.out.println("East");
+			else if (N <= 225) System.out.println("South");
+			else if (N < 360) System.out.println("West");
+		}
 	}
 }
