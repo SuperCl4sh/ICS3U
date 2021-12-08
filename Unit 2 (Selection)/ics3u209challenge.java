ad3249738ecae8ea9b5e386cea5afb45491623ac
Seyon Sivatharan <superclash554@gmail.com>
2021-12-08 09:58:33 -0500

    Add useful comments

--- Unit 2 (Selection)/ics3u209challenge.java
+++ Unit 2 (Selection)/ics3u209challenge.java
@@ -1,10 +1,10 @@
+// Deterministic solution
 import java.io.*;
 import java.util.*;
 
 public class ics3u209challenge {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int A = sc.nextInt();
 		int B = sc.nextInt();
 		int C = sc.nextInt();
--- Unit 2 (Selection)/ics3u209challengev2.java
+++ Unit 2 (Selection)/ics3u209challengev2.java
@@ -1,3 +1,4 @@
+// Probabalistic solution
 import java.io.*;
 import java.util.*;
 
