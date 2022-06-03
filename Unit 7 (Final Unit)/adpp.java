cc9057ca54ca7cd72178dd28772d9448cf508a76
Seyon Sivatharan <superclash554@gmail.com>
2022-06-03 09:59:16 -0400

    Add comments to final assignment and remove useless functions

--- Unit 7 (Final Unit)/adpp.java
+++ Unit 7 (Final Unit)/adpp.java
@@ -3,65 +3,56 @@ import java.util.*;
 
 public class adpp {
     public static final Scanner sc = new Scanner(System.in);
-    public static final int MAX_VAL = (int)Math.pow(10, 4);
+    public static final int MAX_VAL = (int)Math.pow(10, 3); // Constant for highest value (used for frequency array)
     public static final long MOD =  998244353;
     public static void main(String[] args) {
         int N = sc.nextInt();
         long K = sc.nextLong();
         int Q = sc.nextInt();
-        long[] freq = new long[MAX_VAL + 1];
-        for (int i = 0; i <= MAX_VAL; i++) freq[i] = 0;
-        for (int i = 0; i < N; i++) freq[sc.nextInt()]++;
-        freq[0] = 1;
-        long[] ans = new long[MAX_VAL + 1];
-        freq[0] = 1;
+        long[] freq = new long[MAX_VAL + 1]; // Initialize frequency array
+        for (int i = 0; i <= MAX_VAL; i++) freq[i] = 0; // Set frequency array to 0
+        for (int i = 0; i < N; i++) freq[sc.nextInt()]++; // Build frequency array
+        freq[0] = 1; // Set frequency of 0 to 1 since there's 1 way to pick 0 coins
+        /*
+            Freq stores the generating function of the coin with the ith index storing the coefficient of the ith power. For example,
+            (1x^0+1x+1x^2) for the sample input. The coefficient indicates the number of ways to achieve the ith power and the ith power indicates the number of coins.
+            Since there are K rounds, we raise this polynomial to the power of K, ensuring that the size of the polynomial does not exceed MAX_VAL + 1.
+            
+        */
+        long[] ans = new long[MAX_VAL + 1]; // Create answer array
         boolean gone = false;
-        while (K > 0) {
+        while (K > 0) { // Binary exponeniate the polynomial to solve in log(K) time (normally K time)
             if (K % 2 > 0) {
                 if (!gone) {
                     ans = Arrays.copyOfRange(freq, 0, MAX_VAL + 1);
                     gone = true;
                 }
                 else {
-                    long[] res = karatsuba(ans, freq);
-                    //printPolynomial(res);
-                    //printPolynomial(multiply(ans, freq));
-                    //_assert(Arrays.equals(res, multiply(ans, freq)));
-                    ans = Arrays.copyOfRange(res, 0, MAX_VAL + 1); 
+                    long[] res = karatsuba(ans, freq); // Multiply the 2 polynomials using Karatsuba
+                    ans = Arrays.copyOfRange(res, 0, MAX_VAL + 1); // Take only the first MAX_VAL + 1 elements
                 }
             }
-            long[] prodRes = karatsuba(freq, freq);
-            //_assert(Arrays.equals(prodRes, multiply(freq, freq)));
-            //printPolynomial(multiply(freq, freq));
-            //System.out.println();
-            //printPolynomial(prodRes);
-            //System.out.println();
-            freq = Arrays.copyOfRange(prodRes, 0, MAX_VAL + 1); 
-            K >>= 1;
+            long[] prodRes = karatsuba(freq, freq); // Exponentiate the base
+            freq = Arrays.copyOfRange(prodRes, 0, MAX_VAL + 1);  // Take only the first MAX_VAL + 1 elements
+            K >>= 1; // Essentially, floor division by 2
         }
-        //for (int i = 0; i <= MAX_VAL; i++) System.out.print(ans[i] + " ");
-        //System.out.println();
         long[] psa = new long[MAX_VAL + 1];
         psa[0] = ans[0];
-        for (int i = 1; i <= MAX_VAL; i++) psa[i] = (psa[i - 1] + ans[i]) % MOD;
+        for (int i = 1; i <= MAX_VAL; i++) psa[i] = (psa[i - 1] + ans[i]) % MOD; // Calculate PSA (prefix sum array) of the coefficients to answer queries efficiently
         for (int i = 0; i < Q; i++) {
             int L = sc.nextInt(), R = sc.nextInt();
-            System.out.println((psa[R] - ((L == 0) ? 0 : psa[L - 1]) + MOD) % MOD);
+            System.out.println((psa[R] - ((L == 0) ? 0 : psa[L - 1]) + MOD) % MOD); // Output answer
         }
         return;
     }
 
+    // Helper function to debug (Java assert is disabled)
     public static void _assert(boolean condition) {
         if (!condition) throw new RuntimeException("Assert failed!");
         return;
     }
 
-    public static void printPolynomial(long[] A) {
-        for (int i = 0; i < A.length; i++) System.out.print(A[i] + "x^" + i + " ");
-        System.out.println();
-        return;
-    }
-
+    // Function to pad a polynomial A with zeros
     public static long[] addZero(long[] A, int m) {
         long[] res = new long[A.length + m];
         for (int i = 0; i < m; i++) res[i] = 0;
@@ -69,33 +60,29 @@ public class adpp {
         return res;
     }
 
+    // Function to make arrays elements in the range [0, MOD)
     public static long[] fix(long[] A) {
         for (int i = 0; i < A.length; i++) {
-            A[i] += MOD;
-            _assert(A[i] >= 0);
-            A[i] %= MOD;
+            A[i] += MOD; // Numbers after subtraction can be < 0 (element of [-MOD + 1, -1]), so move it back into the range [0, MOD)
+            _assert(A[i] >= 0); // Numbers should always be >= 0
+            A[i] %= MOD; // Take the number under MOD to ensure that it doesn't overflow
         }
         return A;
     }
 
+    // Function to naively multiply two polynomials
     public static long[] multiply(long[] A, long[] B) {
         long res[] = new long[A.length + B.length - 1];
-        //for (int i = 0; i < A.length; i++) System.out.print(A[i] + " ");
-        //System.out.println();
-        //for (int i = 0; i < B.length; i++) System.out.print(B[i] + " ");
-        //System.out.println();
         for (int i = 0; i < res.length; i++) res[i] = 0;
         for (int i = A.length - 1; i >= 0; i--) {
             for (int j = B.length - 1; j >= 0; j--) {
                 res[i + j] += A[i] * B[j] % MOD;
             }
         }
-        //for (int i = 0; i < res.length; i++) System.out.print(res[i] + " ");
-        //System.out.println();
-        //System.out.println();
         return fix(res);
     }
 
+    // Function to add two add two polynomials
     public static long[] add(long[] A, long[] B) {
         long[] res = new long[Math.max(A.length, B.length)];
         int i = 0;
@@ -111,6 +98,7 @@ public class adpp {
         return fix(res);
     }
 
+    // Function to subtract two polynomials
     public static long[] subtract(long[] A, long[] B) {
         long[] res = new long [Math.max(A.length, B.length)];
         int i;
@@ -126,20 +114,18 @@ public class adpp {
         return fix(res);
     }
 
+    // Function to multiply two polynomials efficiently in ~O(N^{log_{2}3})~ time (approximately N^{1.5})
     public static long[] karatsuba(long[] A, long[] B) {
-        //return multiply(A, B);
-        if (A.length < 3 || B.length < 3) return multiply(A, B);
-        int m = Math.min(A.length, B.length) / 2;
+        if (A.length < 3 || B.length < 3) return multiply(A, B); // Base case: multiply the polynomials naively
+        int m = Math.min(A.length, B.length) / 2; // Get half length
+        // Divide the array into 4 relevant segments
         long leftA[] = Arrays.copyOfRange(A, 0, m), rightA[] = Arrays.copyOfRange(A, m, A.length);
         long leftB[] = Arrays.copyOfRange(B, 0, m), rightB[] = Arrays.copyOfRange(B, m, B.length);
+        // Call Karatsuba on the new, smaller arrays
         long[] z0 = karatsuba(leftA, leftB);
-        //System.out.println("_______");
-        //printPolynomial(z0);
-        //printPolynomial(multiply(leftA, leftB));
-        //_assert(Arrays.equals(z0, multiply(leftA, leftB)));
-        //System.out.println("_______");
         long[] z1 = karatsuba(rightA, rightB);
         long[] z2 = subtract(karatsuba(add(leftB, rightB), add(leftA, rightA)), add(z0, z1));
+        // Pad the final answer accordingly with zeros
         return add(z0, add(addZero(z1, 2 * m), addZero(z2, m)));
     }
 }
